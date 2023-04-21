5ccaf764e8dd7bd7272c13242902de6e528f040b
Miguel Santana <miguel@savesolarcorporation.com>
2023-04-21 18:31:28 -0500

    update apiEndpoint with ES6 export

--- src/api/apiEndpoint.js
+++ src/api/apiEndpoint.js
@@ -1,5 +1,1 @@
-// module.exports = {
-//     apiEndpoint: 'https://tqdljhnexk.execute-api.us-east-1.amazonaws.com/prodd'
-// }
-
-export const apiEndpoint = 'https://tqdljhnexk.execute-api.us-east-1.amazonaws.com/prodd'
+export const apiEndpoint = 'https://tqdljhnexk.execute-api.us-east-1.amazonaws.com/prod'
